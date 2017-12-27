0bc9749d872b9e297d9bb60b5b45b6b314274a7b
azharvaltrans <azhar@valtrans.org>
2017-12-27 16:13:26 +0400

    update with latest config

--- db_config.js
+++ db_config.js
@@ -1,25 +1,32 @@
 var mysql      = require('mysql')
-var env = 'dev'
 
-if(env == 'local')
-{
-	$connection_info = {
-	    host     : 'localhost',
-	  	port     : '3306',
-	    user     : 'valtrans',
-	    password : 'valtrans@123',
-	    database : 'parkonic_dev'
+var connection_info = function (event) {
+  	if(event.principalId)
+	{
+		$connection_info = {
+		    host     : 'localhost',
+		  	port     : '3306',
+		    user     : 'valtrans',
+		    password : 'valtrans@123',
+		    database : 'parkonic_dev'
+		}
+
+	} else {
+		$connection_info = {
+		    host     : 'valtransdb.cqlt28njzgkt.eu-west-1.rds.amazonaws.com',
+		    user     : 'valtransDB',
+		    password : 'ValTrans17',
+		    database: 'parkonic_dev'
+		}
 	}
 
-} else {
-	$connection_info = {
-	    host     : 'valtransdb.cqlt28njzgkt.eu-west-1.rds.amazonaws.com',
-	    user     : 'valtransDB',
-	    password : 'ValTrans17',
-	    database: 'parkonic_dev'
-	}
+	return $connection_info;
 }
-var connection = mysql.createConnection($connection_info)
 
-connection.connect()
-module.exports = connection
+
+exports.connection = ( event ) => {
+	return mysql.createConnection(connection_info(event))
+}
+
+// connection.connect()
+// exports.connection = connection
--- handler.js
+++ handler.js
@@ -4,11 +4,14 @@ if (!global._babelPolyfill) {
    require('babel-polyfill')
 }
 
-var connection = require("./db_config.js")
+var config = require("./db_config.js")
 
 export const users = (event, context, callback) => {
+  var connection = config.connection(event)
+  connection.connect()
+  
   var query="SELECT * FROM users"
-
+  console.info(connection);
   connection.query(query,function(err,rows) {
       const response = {
           statusCode: 200,
